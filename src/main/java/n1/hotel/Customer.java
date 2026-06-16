c5f627db548a40ca24eccdb00f528ee4724c2ed2
tsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-06-16 16:08:16 +0300

    feat: Add customer class

---
+++ src/main/java/n1/hotel/Customer.java
@@ -0,0 +1,10 @@
+package n1.hotel;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+}
